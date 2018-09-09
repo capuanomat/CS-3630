7a0d6fe8c6d63e097a6842fc53a22662da5e9f5e
Daphne Chen <daphne.chen96@gmail.com>
2018-09-09 00:38:47 -0400

    added training data and modified state_machine.py; image classification works sometimes in the arena with even lighting

--- state_machine.py
+++ state_machine.py
@@ -3,6 +3,8 @@ import sys
 import cozmo
 import datetime
 import time
+import numpy as np
+from skimage import color
 
 from lab1 import *
 
@@ -14,20 +16,34 @@ def run(sdk_conn):
 
     img_clf = ImageClassifier()
     train_classifier(img_clf)
+    test_classifier(img_clf)
 
     while True:
-        time.sleep(0.5)
+        time.sleep(2)
         latest_image = robot.world.latest_image
-        new_image = latest_image.raw_image
+        new_image = np.array(latest_image.raw_image)
 
-        predicted_label = img_clf.predict_labels(new_image)
+        print("......................----------")
+        print(new_image.shape)
+
+        arr_image = np.array([new_image])
+
+        print("......................----------")
+        print(arr_image.shape)
+
+        image_features = img_clf.extract_image_features(arr_image)
+
+        
+        predicted_label = img_clf.predict_labels(image_features)
 
         print(predicted_label)
 
-        robot.say_text(predicted_label).wait_for_completed()
+        robot.say_text(predicted_label[0]).wait_for_completed()
 
         timestamp = datetime.datetime.now().strftime("%dT%H%M%S%f")
-        new_image.save("_DAPHNE_IS_A_DAPHNE_" + timestamp + ".jpg")
+
+        temp_image = latest_image.raw_image
+        temp_image.save("_DAPHNE_IS_A_DAPHNE_" + str(predicted_label[0]) + timestamp + ".jpg")
 
 
 def train_classifier(img_clf):
@@ -37,8 +53,24 @@ def train_classifier(img_clf):
     # convert images into features
     train_data = img_clf.extract_image_features(train_raw)
 
+    print(train_data)
+
     # train model and test on training data
     img_clf.train_classifier(train_data, train_labels)
+
+def test_classifier(img_clf):
+
+    (test_raw, test_labels) = img_clf.load_data_from_folder('./test/')
+
+    test_data = img_clf.extract_image_features(test_raw)
+
+    predicted_labels = img_clf.predict_labels(test_data)
+
+    print("\nTraining results")
+    print("=============================")
+    print("Confusion Matrix:\n",metrics.confusion_matrix(test_labels, predicted_labels))
+    print("Accuracy: ", metrics.accuracy_score(test_labels, predicted_labels))
+    print("F1 score: ", metrics.f1_score(test_labels, predicted_labels, average='micro'))
 
 if __name__ == '__main__':
     cozmo.setup_basic_logging()
